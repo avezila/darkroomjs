9ea9733236883c1ade77d0de1f210b2931d7ad33
Pete Alexandrou <pete@ozmartians.com>
2016-04-10 01:46:51 +0800

    .

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 gulp build
 
-cp build/darkroom.css /home/ozmartian/Projects/ocr-translator/css
-cp build/darkroom.js /home/ozmartian/Projects/ocr-translator/js
+cp build/darkroom.css ../ocr-translator/css
+cp build/darkroom.js ../ocr-translator/js
 
 exit 0
